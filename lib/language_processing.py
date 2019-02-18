98a82ee27bbc70a69020eaaa5e63c69711542a80
danhaus <danhaus95@gmail.com>
2019-02-18 16:36:36 +0000

    [REFACTOR] making classes in language_processing.py reusable for title as well

--- lib/language_processing.py
+++ lib/language_processing.py
@@ -7,8 +7,10 @@ class StemmedTokenizer():
     def __init__(self):
         pass
 
-    def tokenize(self, df):
+    def tokenize(self, df, column_name):
     	"""
+        df: DataFrame that contains column with text to be tokenized
+        column_name: name of the column to be tokenized (string)
     	Stems and tokenizes all the text in the item description.
     	Returns a dictionary that maps item id to a dictionary storing the stemmed word and its count (for the fiven item)
     	"""
@@ -20,7 +22,7 @@ class StemmedTokenizer():
 
     	stemmed_tokens_d = {} # dict to store words and their count (as dict) under item id (train_id)
     	# Iterate through the items
-    	for id_, text in df.item_description.iteritems():
+    	for id_, text in df[column_name].iteritems():
     		# Iterate through the sentences
     		for sentence in sent_tokenize(text):
     			# Iterate through the words
@@ -38,9 +40,15 @@ class StemmedTokenizer():
 
 class CountVectorizer(StemmedTokenizer):
 
-    def __init__(self, df_train):
+    def __init__(self, df_train, column_name):
+        """
+        df_train: DataFrame to be processed to create vocabulary set whose
+            content will be used for tokenizing
+        column_name: name of the column containg text to be tokenized
+        """
         self.df_train = df_train
-        self.train_stemmed_tokens = self.tokenize(df_train)
+        self.column_name = column_name
+        self.train_stemmed_tokens = self.tokenize(df_train, self.column_name)
         self.voc_set = self.create_voc_set() # keep set to speed up look ups
         self.voc_set_lst = list(self.voc_set) # this is the base for the word vectors
 
@@ -48,7 +56,7 @@ class CountVectorizer(StemmedTokenizer):
         # Create vocabulary set
         voc_set_lst = self.voc_set_lst
         X = pd.DataFrame(0, index=df.index, columns=voc_set_lst)
-        stemmed_tokens_d = self.tokenize(df)
+        stemmed_tokens_d = self.tokenize(df, self.column_name)
         # Iterate through the ids
         for id_, word in stemmed_tokens_d.items():
             # Iterate through words and their respective counts
