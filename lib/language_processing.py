17c10d1d8a58ccee9e11402238acf9473ef33acf
danhaus <danhaus95@gmail.com>
2019-03-04 21:29:33 +0000

    [FIX][ADD] Fixed incorrect tokenization. Added PrincipalAxesExtractor
    The tokenization took only the last sentence into account by mistake

--- lib/language_processing.py
+++ lib/language_processing.py
@@ -1,6 +1,7 @@
 from nltk.stem import PorterStemmer
 from nltk.tokenize import sent_tokenize, word_tokenize
 import pandas as pd
+from sklearn.decomposition import PCA
 
 
 
@@ -19,7 +20,8 @@ class Tokenizer():
         df: DataFrame that contains column with text to be tokenized
         column_name: name of the column to be tokenized (string)
         Stems and tokenizes all the text in the item description.
-        Returns a dictionary that maps item id to a dictionary storing the processed words and their count (for the given item)
+        Returns a dictionary that maps item id to a dictionary storing
+        the processed words and their respective count (for the given item)
         """
 
         # Construct dictionary with processed words for every item in the category
@@ -27,9 +29,9 @@ class Tokenizer():
         # Iterate through the items
         for id_, text in df[column_name].iteritems():
             # Iterate through the sentences
+            words_d = {}
             for sentence in sent_tokenize(text):
                 # Iterate through the words
-                words_d = {}
                 for word in word_tokenize(sentence):
                     if self.stem:
                         processed_word = self.ps.stem(word)
@@ -42,6 +44,8 @@ class Tokenizer():
                         words_d[processed_word] += 1
                 processed_tokens_d[id_] = words_d
         return processed_tokens_d
+
+    # TODO: Implement stopwords
 
     def create_voc_set(self, processed_tokens_d):
         """
@@ -144,8 +148,7 @@ class MeanEmbeddingVectorizer(Tokenizer):
                     X.at[id_, pre_name + word] = self.model[word].mean()
         return X
 
-
-    ### Helper functions ###
+    ### Helper methods ###
 
     def create_intersect_voc_set(self):
         """
@@ -157,3 +160,78 @@ class MeanEmbeddingVectorizer(Tokenizer):
         self.voc_set_model = voc_set_model # for debugging
         voc_intersect = voc_set_df_train.intersection(voc_set_model) # get intersection of the two sets
         return voc_intersect
+
+
+
+class PrincipalAxesExtractor(Tokenizer):
+    """
+    Tokenizes text, and uses word2vec pretrain model to create a matrix
+    with a shape of (n_of_model_dimensions, n_of_words). Then it applies SVD
+    to find the most important directions (vectors) of variation. Next, it uses
+    these directions as features as follows: 1) select N most important directions
+    of variation, 2) unroll them to create columns / features, 3) populate
+    the columns with the data for each item / row. (The columns goes as:
+    vector1_dir1, ...vector1_dirN, vector2_dir1, ...)
+    """
+    def __init__(self, model, n_directions, column_name):
+        """
+        model: trained word2vec model (usually stored in .word2vec file) loaded
+            using gensim
+        n_directions: number of most significant directions in variation to select
+            (most significat vectors from the U matrix after SVD decomposition)
+        column_name: name of the column containg text to be processed
+        """
+        super().__init__(stem=False) # initialize the Tokenizer without stemming
+        self.model = model
+        self.n_directions = n_directions
+        self.column_name = column_name
+        self.pca = PCA(n_components=n_directions)
+        self.output_column_name_patern = 'princ_axis_{}_dim_{}'
+        self.output_column_names = self.create_column_names(self.output_column_name_patern)
+
+    def extract(self, df):
+        pca = self.pca
+        output_column_name_patern = self.output_column_name_patern
+        n_directions = self.n_directions
+        X = pd.DataFrame(0, index=df.index, columns=self.output_column_names, dtype='float32')
+        # Get word vectors
+        word_vectors_dict = self.create_word_vectors(df)
+
+        for id_, words in word_vectors_dict.items():
+            # DataFrame shape: (Nth vector component, word)
+            # number of rows = model.vector_size
+            # number of columns = number of words
+            word_vectors_df = pd.DataFrame.from_dict(words)
+            # If the number of words is too small to perform pca, leave all the values
+            # set to zeros and skip to the next id_
+            if word_vectors_df.shape[1] < n_directions:
+                continue
+            word_vectors_df_transposed = word_vectors_df.transpose()
+            pca.fit(word_vectors_df_transposed)
+            X.loc[id_] = pca.components_.ravel()
+        return X
+
+    ### Helper methods ###
+
+    def create_word_vectors(self, df):
+        model = self.model
+        processed_tokens_d = self.tokenize(df, self.column_name)
+        self.processed_tokens_d = processed_tokens_d # DEBUGGING
+        word_vectors_dict = {} # {item_id: {word: word_vector}} only for words in the model voc
+        for id_, words in processed_tokens_d.items():
+            word_vectors_dict[id_] = {}
+            for word in words.keys():
+                # Only get the word vector if it is in the model vocabulary
+                if word in model.vocab:
+                    word_vectors_dict[id_][word] = model[word]
+        self.word_vectors = word_vectors_dict # DEBUGGING
+        return word_vectors_dict
+
+    def create_column_names(self, output_column_name_patern):
+        n_directions = self.n_directions
+        vector_size = self.model.vector_size # number of dimensions of the model voc
+        output_column_names = []
+        for comp_n in range(n_directions):
+            for dim in range(vector_size):
+                output_column_names.append(output_column_name_patern.format(comp_n, dim))
+        return output_column_names
