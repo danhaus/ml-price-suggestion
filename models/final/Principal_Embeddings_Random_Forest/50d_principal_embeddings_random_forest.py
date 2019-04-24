0db3cd60bd5149b73ef6ce1c3c1dcdb78c7ebc98
Dan Hausner <dan.hausner@proofer.com>
2019-04-24 13:55:30 +0100

    second run

--- models/final/Principal_Embeddings_Random_Forest/50d_principal_embeddings_random_forest.py
+++ models/final/Principal_Embeddings_Random_Forest/50d_principal_embeddings_random_forest.py
@@ -48,16 +48,16 @@ from sklearn.model_selection import ParameterGrid
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 
-n_directions_lst = [1, 2, 3, 4]  # first run
-# n_directions_lst = [4, 7, 10]  # second run
+# n_directions_lst = [1, 2, 3, 4]  # first run
+n_directions_lst = [1, 2, 3, 4, 5, 6, 7, 8]  # second run
 NLP_param_grid = {
     'desc_sw': [stopwords, None], 'desc_stem': [True, False], 'desc_n_dirs': n_directions_lst,
     'name_sw': [stopwords, None], 'name_stem': [True, False], 'name_n_dirs': n_directions_lst,
 }
 
 grid_search_param_grid = {
-    'n_estimators': [1, 3, 10, 30, 100, 300, 1000],  # first run
-    # 'n_estimators': [100, 300, 1000],  # second run
+    # 'n_estimators': [1, 3, 10, 30, 100, 300, 1000],  # first run
+    'n_estimators': [30, 100, 300, 1000],  # second run
     'max_depth': [1, 3, 10, 30, 100],
     'min_samples_leaf': [1, 3, 10],
     'random_state': [42]
@@ -123,5 +123,5 @@ optimisation_result['y_train'] = y_train
 optimisation_result['y_test'] = y_test
 
 print("Saving pickle")
-hf.save_pickle(optimisation_result, f'{PATH}pickle/50d_PE_optimisation_result')
+hf.save_pickle(optimisation_result, f'{PATH}pickle/second_50d_PE_optimisation_result')
 print("Done")
